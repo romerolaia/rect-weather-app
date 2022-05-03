df46312364760715bafe39a269f9ef60eb2dc485
romerolaia <romerocarollaia@gmail.com>
2022-05-04 01:07:58 +0200

    updated js

--- src/ActualDate.js
+++ src/ActualDate.js
@@ -1,19 +1,23 @@
-import React from 'react';
-
+import React from "react";
 
 export default function ActualDate(props) {
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let day = days[props.date.getDay()];
 let hours = props.date.getHours();
+
 if(hours< 10){
 hours = `0${hours}` ;
 }
 
 let minutes = props.date.getMinutes();
+
 if(minutes < 10){
-    minutes = `0${minutes}` 
+    minutes = `0${minutes}`;
 }
 
-return `${day} ${hours}:${minutes}`;
+return (
+<div>
+    {day} {hours}:{minutes}
+</div>);
 }
