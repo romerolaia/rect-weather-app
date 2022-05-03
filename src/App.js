3726f8ec5d656b4e71f15c2a78552b9d2ace2fe9
romerolaia <romerocarollaia@gmail.com>
2022-05-03 17:36:57 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
     <footer>
 This project was coded by Laia Romero and is {" "}
     <a href="https://github.com/romerolaia/rect-weather-app" target="_blank">open-sourced on GitHub</a>
     </footer>
+    </div>
     </div>
   );
 }
