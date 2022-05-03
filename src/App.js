a93b168b495180333eb3bea459491a48d1ecbab4
romerolaia <romerocarollaia@gmail.com>
2022-05-03 17:08:52 +0200

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
+    <footer>
+This project was coded by Laia Romero and is {" "}
+    <a href="https://github.com/romerolaia/rect-weather-app" target="_blank">open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
